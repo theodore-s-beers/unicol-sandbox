ef42174199ea83e9a2c71793a1313a958211331e
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-07-03 18:35:07 +0200

    Pass second CLDR test

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use std::cmp::Ordering;
 use unicol_sandbox::{compare_sort_keys, str_to_sort_key, CollationOptions, KeysSource};
 
+#[allow(clippy::too_many_lines)]
 fn main() {
     //
     // DUCET, NON-IGNORABLE
@@ -16,9 +17,9 @@ fn main() {
         shifting: false,
     };
 
-    let mut max_sk: Vec<u16> = Vec::new();
-
     let start = std::time::Instant::now();
+
+    let mut max_sk: Vec<u16> = Vec::new();
 
     for line in test_data.lines() {
         if line.is_empty() || line.starts_with('#') {
@@ -65,9 +66,9 @@ fn main() {
         shifting: true,
     };
 
-    let mut max_sk = Vec::new();
-
     let start = std::time::Instant::now();
+
+    let mut max_sk = Vec::new();
 
     for line in test_data.lines() {
         if line.is_empty() || line.starts_with('#') {
@@ -115,9 +116,9 @@ fn main() {
         shifting: false,
     };
 
-    let mut max_sk = Vec::new();
-
     let start = std::time::Instant::now();
+
+    let mut max_sk = Vec::new();
 
     for line in test_data.lines() {
         if line.is_empty() || line.starts_with('#') {
@@ -150,5 +151,55 @@ fn main() {
     let total_lines = test_data.lines().count();
 
     println!("Passed CollationTest_CLDR_NON_IGNORABLE");
+    println!("Compared {} lines in {:?}", total_lines, duration);
+    println!();
+
+    //
+    // CLDR, SHIFTED
+    //
+
+    let test_data =
+        std::fs::read_to_string("test-data/CollationTest_CLDR_SHIFTED_SHORT.txt").unwrap();
+
+    let options = CollationOptions {
+        keys_source: KeysSource::Cldr,
+        shifting: true,
+    };
+
+    let start = std::time::Instant::now();
+
+    let mut max_sk = Vec::new();
+
+    for line in test_data.lines() {
+        if line.is_empty() || line.starts_with('#') {
+            continue;
+        }
+
+        let hex_values: Vec<&str> = line.split(' ').collect();
+        let mut test_string = String::new();
+
+        for s in hex_values {
+            let val = u32::from_str_radix(s, 16).unwrap();
+            // This is BS, but we have to use an unsafe method because the tests deliberately
+            // introduce invalid character values.
+            let c = unsafe { std::char::from_u32_unchecked(val) };
+            test_string.push(c);
+        }
+
+        let sk = str_to_sort_key(&test_string, &options);
+
+        let comparison = compare_sort_keys(&sk, &max_sk);
+        if comparison == Ordering::Less {
+            panic!();
+        }
+
+        max_sk = sk;
+    }
+
+    let duration = start.elapsed();
+
+    let total_lines = test_data.lines().count();
+
+    println!("Passed CollationTest_CLDR_SHIFTED");
     println!("Compared {} lines in {:?}", total_lines, duration);
 }
