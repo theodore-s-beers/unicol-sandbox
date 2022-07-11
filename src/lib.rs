5b7957ea40f91975e73a6e4e7718d22e2c8f8b2f
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-07-11 10:47:02 +0200

    Simplify prefix logic

--- src/lib.rs
+++ src/lib.rs
@@ -222,14 +222,9 @@ fn trim_prefix(a: &mut Vec<u32>, b: &mut Vec<u32>, cldr: bool) {
     let prefix_len = find_prefix(a, b);
 
     if prefix_len > 0 {
-        for elem in &a[0..prefix_len] {
-            if NEED_THREE.contains(elem) || NEED_TWO.contains(elem) {
-                return;
-            }
-        }
-
         let sing = if cldr { &SING_CLDR } else { &SING };
 
+        // Test final code point in prefix; bail if bad
         if let Some(row) = sing.get(&a[prefix_len - 1]) {
             for weights in row {
                 if weights.variable || weights.primary == 0 {
@@ -244,7 +239,10 @@ fn trim_prefix(a: &mut Vec<u32>, b: &mut Vec<u32>, cldr: bool) {
 }
 
 fn find_prefix(a: &[u32], b: &[u32]) -> usize {
-    a.iter().zip(b).take_while(|(x, y)| x == y).count()
+    a.iter()
+        .zip(b)
+        .take_while(|(x, y)| x == y && !NEED_THREE.contains(x) && !NEED_TWO.contains(x))
+        .count()
 }
 
 fn nfd_to_sk(nfd: &mut Vec<u32>, opt: CollationOptions) -> Vec<u16> {
